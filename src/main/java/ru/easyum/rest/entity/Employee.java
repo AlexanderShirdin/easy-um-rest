9c99c72635619249cfb61759b85665d836b6124c
AlexanderShirdin <cpt.blak@gmail.com>
2022-05-26 19:47:56 +0300

    проект Spring

--- src/main/java/ru/easyum/rest/entity/Employee.java
+++ src/main/java/ru/easyum/rest/entity/Employee.java
@@ -27,5 +27,7 @@ public class Employee {
     message = "Invalid Number")
     private String number;
     @EmailCheck
+    @Pattern(regexp ="^[a-zA-Z0-9_+&*-]+(\\.[a-zA-Z0-9_+&*-]+)*@([spring-]+\\.)[com]{2,7}$", message = "Invalid Email")
     private String email;//spring.com
+
 }
